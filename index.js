d7e4c379503fe57b307f426de22e2ad76e20f686
Morgan Herlocker <morgan.herlocker@gmail.com>
2025-02-21 17:01:13 -0800

    z3/z4

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-for (let y = 0; y < 4; y++) {
-  for (let x = 0; x < 4; x++) {
-    const minlon = x * 90 - 180;
-    const minlat = -(y - 4) * 45 - 135;
-    const maxlon = minlon + 90;
-    const maxlat = -(y - 4) * 45 - 90;
+const z = 4;
+const lonsize = 360 / 2 ** z;
+const latsize = 180 / 2 ** z;
+
+for (let y = 0; y < 2 ** z; y++) {
+  for (let x = 0; x < 2 ** z; x++) {
+    const minlon = x * lonsize - 180;
+    const maxlat = -(y - 2 ** z) * latsize - 90;
+    const minlat = maxlat - latsize;
+    const maxlon = minlon + lonsize;
     console.log(
-      `mkdir -p ./2/${x}; ./globe render -i ./globe.bin -o ./2/${x}/${y}.png --minlon=${minlon} --minlat=${minlat} --maxlon=${maxlon} --maxlat=${maxlat};`,
+      `mkdir -p ./${z}/${x}; time ./globe render -i ./globe.bin -o ./${z}/${x}/${y}.png --minlon=${minlon} --minlat=${minlat} --maxlon=${maxlon} --maxlat=${maxlat};`,
     );
   }
 }
-
-//➜  globe-c git:(gh-pages) ✗ ./globe render -i ./globe.bin -o ./demo/1/1/0.png --minlon=0 --minlat=0 --maxlon=180 --maxlat=90;
